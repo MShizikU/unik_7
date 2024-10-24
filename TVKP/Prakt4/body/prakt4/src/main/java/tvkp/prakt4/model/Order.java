1f37e51dffa907d8509834077d69002717ae2870
Stas Sidorov <SSidorov@croc.ru>
2024-10-24 20:40:06 +0300

    tvkp prakt4

---
+++ TVKP/Prakt4/body/prakt4/src/main/java/tvkp/prakt4/model/Order.java
@@ -0,0 +1,29 @@
+package tvkp.prakt4.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToMany
+    @JoinTable(
+            name = "order_products",
+            joinColumns = @JoinColumn(name = "order_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id")
+    )
+    private List<Product> products;
+}
+
---
+++ TVKP/Prakt4/body/prakt4/src/main/java/tvkp/prakt4/model/User.java
@@ -0,0 +1,22 @@
+package tvkp.prakt4.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+    @OneToMany(mappedBy = "user")
+    private List<Order> orders;
+}
